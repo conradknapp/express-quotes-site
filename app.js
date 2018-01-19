000835c9e7fce7222f3fa4c118651b9582679159
Conrad Knapp <conrad.knapp@bc.edu>
2018-01-19 16:03:06 -0500

    Adds Google OAuth Strategy

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const passport = require('passport');
+
+require('./config/passport')(passport);
+
+const auth = require('./routes/auth');
 
 const app = express();
+
+app.use('/auth', auth);
 
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`Listening on ${port}`);
+});
+
+app.get('/', (req, res) => {
+  res.send('it works');
 });
