572e00fd109cc399ae3543e9aed45d87d71f3c24
Conrad Knapp <35019562+conradknapp@users.noreply.github.com>
2018-04-07 18:46:23 -0400

    Delete configKeys.js

--- configKeys.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  mongoURI: 'mongodb://cknapp92:Spike1992@ds263317.mlab.com:63317/express-quotes-site',
-  googleClientID: '261585975330-728bg11mdlrstpt69niscmqj407s544v.apps.googleusercontent.com',
-  googleClientSecret: 'BBQdR2QssbfTKSMa7TnZxblv'
-}
